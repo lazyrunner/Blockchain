44ee3cb9c11697cf916c7b042706d86c2b92645b
lazyrunner <joshua.mark92@gmail.com>
2021-08-22 18:37:21 +0530

    Create basic candidate and deploy

--- README.md
+++ README.md
@@ -11,3 +11,23 @@ Extention Connect to a blockchain
 
 ## Smart contract
 written in solidity
+
+
+## Deploy contract
+``` truffle migrate --reset ```
+Enter console
+``` truffle console ```
+Get instance of app
+``` Election.deployed().then(function(instance) { app=instance;} ) ```
+
+Get candidates
+``` app.candidates(1) ```
+``` app.candidates(2) ```
+``` app.candidatesCount() ```
+
+Get candidate
+``` app.candidates(1).then(function(c) {candidate = c;}) ```
+``` candidate ```
+
+
+https://youtu.be/3681ZYbDSSk?t=2634
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,15 +1,29 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    //Store Candidate 
-    //Read candidate
-    // Constructor - run when deploy into blockchain
-    
-    //state variable accessible inside contract and represent data
-    string public candidate;
+    //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Store Candidate
+    //mapping is like associative array(hash)
+    mapping(uint => Candidate) public candidates;
+
+    //Store Candidate Count
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 
+    //Adding a candidate
+    //private so only contract can access
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
